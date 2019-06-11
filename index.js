4e929c7783f7a836fc877bc0a462b3971a92f74e
savyatam <44085001+savyatam@users.noreply.github.com>
2019-06-11 22:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+ /* socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
+  });*/
+  console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
 });
 
 http.listen(port, function(){
